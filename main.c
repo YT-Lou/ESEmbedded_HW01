af08b951fbadb4b00b605f86071429936a2200e0
Y-T Lou <kacz0981z@gmail.com>
2019-03-06 23:38:51 +0800

    function pointer_za1

--- main.c
+++ main.c
@@ -7,17 +7,9 @@ int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
 int main()
 {
-	int a, c;
-	char b;
-	printf("key a Function \nEX: 1 + 1\n");
-	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+        printf("key a Function \nEX: 1 + 1\n");
+        scanf("%i %c %i", &a, &b, &c);
+        int (*z1a)(int a,char b,int c) = {plus,minus,multiply,divided}
+        int main()
+        printf("%d %c %d = %d\n", a, b, c, z1a(a, c));
 }
